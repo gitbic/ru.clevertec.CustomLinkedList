677f69dd1928e60bce4fbe508a1b71a36bf747d4
Pavel Turski <pavelitelb@gmail.com>
2021-02-15 14:52:26 +0300

    clear project

--- src/main/java/ru/clevertec/RunnerLinkedList.java
+++ src/main/java/ru/clevertec/RunnerLinkedList.java
@@ -1,54 +1,18 @@
 package ru.clevertec;
 
-import java.util.*;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.locks.ReadWriteLock;
-import java.util.concurrent.locks.ReentrantReadWriteLock;
+import java.util.List;
 
 public class RunnerLinkedList {
     public static void main(String[] args) {
-//        List<String> list = new CustomLinkedList<>();
-//
-//        for (int i = 0; i < 10; i++) {
-//            list.add("element " + i);
-//        }
-//
-//        System.out.println(list.toString());
+        List<String> list = new CustomLinkedList<>();
 
-
-        List<String> list = new ThreadSafeCustomLinkedList<>();
-
-        for (int i = 0; i < 100000; i++) {
-            list.add("Number: " + i);
+        for (int i = 0; i < 10; i++) {
+            list.add("element " + i);
         }
 
-        Iterator<String> iterator = list.iterator();
+        System.out.println(list.toString());
 
-        IteratorThread<String> thread1 = new IteratorThread<>("Thread 1", iterator);
-        IteratorThread<String> thread2 = new IteratorThread<>("Thread 2", iterator);
-        IteratorThread<String> thread3 = new IteratorThread<>("Thread 3", iterator);
-        IteratorThread<String> thread4 = new IteratorThread<>("Thread 4", iterator);
-        IteratorThread<String> thread5 = new IteratorThread<>("Thread 5", iterator);
-
-
-        try {
-            thread1.start();
-            thread2.start();
-            thread3.start();
-            thread4.start();
-            thread5.start();
-            TimeUnit.SECONDS.sleep(1);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-
-        System.out.println("Element count: "
-                + (thread1.count.get()
-                + thread2.count.get()
-                + thread3.count.get()
-                + thread4.count.get()
-                + thread5.count.get()));
 
     }
+
 }
